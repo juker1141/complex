9a2d3fa088dd3c3899cc28a14b9d378a10f074df
Ryu <ryu@realtime.tw>
2023-03-02 17:53:02 +0800

    update for wrong word

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const pgClient = new Pool({
   host: keys.pgHost,
   database: keys.pgDatabase,
   password: keys.pgPassword,
-  post: keys.pgPort,
+  port: keys.pgPort,
 });
 
 pgClient.on("connect", (client) => {
